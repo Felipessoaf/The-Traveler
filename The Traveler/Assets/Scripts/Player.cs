4f01a32585502a46e220790b9eabcb19c8d20502
Felipe Pessoa <felipessoaf@gmail.com>
2017-09-07 18:19:04 -0300

    walk anim menu

---
+++ The Traveler/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    #region enum
+    enum State
+    {
+        menuWalk,
+        stationary,
+        walking,
+        turning
+    }
+    #endregion
+
+    #region private variables
+    private State _currentState;
+    [SerializeField]
+    private Animator _animator;
+    #endregion
+
+    #region public variables
+
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        _currentState = State.menuWalk;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(_currentState)
+        {
+            case State.menuWalk:
+                break;
+            case State.stationary:
+                break;
+            case State.walking:
+                break;
+            case State.turning:
+                break;
+            default:
+                break;
+        }
+	}
+}
