c2d7b0b853f57c64c693593a0d6427a335f43b95
Felipe Pessoa <felipessoaf@gmail.com>
2017-09-08 01:29:43 -0300

    cam rotation

---
+++ The Traveler/Assets/Scripts/CamController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _rotateMultiplier;
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, _target.rotation,Time.deltaTime * _rotateMultiplier);
+	}
+}
